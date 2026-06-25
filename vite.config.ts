7e5e27382e07a14b913615e2753bcd300d8feb37
amjedq7 <amjedq7.code@gmail.com>
2026-06-25 15:27:09 +0200

    fixing local website

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/web-jahaf/',
-})
+  // Lokálně použije '/', pro GitHub Pages použije '/web-jahaf/'
+  base: command === 'build' ? '/web-jahaf/' : '/',
+}))
