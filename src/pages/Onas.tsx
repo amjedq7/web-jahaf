f2316bde66acf4cdd67dea437fee26e2675d85eb
amjedq7 <amjedq7.code@gmail.com>
2026-06-25 21:55:18 +0200

    edit page Onas

--- src/pages/Onas.tsx
+++ src/pages/Onas.tsx
@@ -46,7 +46,10 @@ export default function Onas() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
                   </svg>
                 </div>
-                <div>
+                <div className="flex flex-col">
+                  <span className="text-sm text-slate-500 font-bold uppercase tracking-wider mb-0.5">
+                    Jazykové znalosti
+                  </span>
                   <p className="font-bold text-slate-900 text-lg md:text-xl">
                     Čeština, Angličtina, Arabština, Němčina
                   </p>
